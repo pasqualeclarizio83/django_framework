a002b46c3906fd63defadec83d7e5640747593b0
pasqualeclarizio83 <pascalclarizio@gmail.com>
2024-03-09 11:16:24 +0100

    dashboard Gestionale

--- dashboard_gestionale_crud3/nuova_app/urls.py
+++ dashboard_gestionale_crud3/nuova_app/urls.py
@@ -6,6 +6,12 @@ from .views import (
 
     DepositoListView,
 
+    DepositoCreateView,
+
+    DepositoUpdateView,
+
+    DepositoDeleteView,
+
 )
 
 urlpatterns = [
@@ -14,5 +20,10 @@ urlpatterns = [
     path("", HomeVuota.as_view(), name="homepage"), #1° PRIMO # pagina1.html
 
     path('depositi/', DepositoListView.as_view(), name='deposito_list'),
+
+    path('add/', DepositoCreateView.as_view(), name='deposito_add'),
+
+    path('<int:pk>/edit/', DepositoUpdateView.as_view(), name='deposito_edit'),
+    path('<int:pk>/delete/', DepositoDeleteView.as_view(), name='deposito_delete'),
 
 ]
